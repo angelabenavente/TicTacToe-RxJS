846bc0daacc39b2bf6c42efb5750fd2aaa96b53d
angelabenavente <angela.b.almazan@gmail.com>
2020-02-21 09:58:59 +0100

    Return user moves

--- src/tic-tac-toe/userMove.js
+++ src/tic-tac-toe/userMove.js
@@ -3,3 +3,15 @@ import { fromEvent } from 'rxjs'
 import { map } from 'rxjs/operators'
 
 
+const click$ = fromEvent(canvas, 'click').pipe(
+  map(val => {
+    return {
+      x: Math.floor(val.offsetX / CELL_SIZE),
+      y: Math.floor(val.offsetY / CELL_SIZE)
+    }
+  })
+);
+
+export const userMove$ = click$.pipe(
+  //filter valid moves
+)
