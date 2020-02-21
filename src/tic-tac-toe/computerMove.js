4719d1c7ddcfb2fd2f5a62a90da6d5f28ae7bde2
angelabenavente <angela.b.almazan@gmail.com>
2020-02-21 11:06:44 +0100

    Change name in jsons

--- src/tic-tac-toe/computerMove.js
+++ src/tic-tac-toe/computerMove.js
@@ -4,6 +4,5 @@ export const computerMove$ = new Subject();
 
 export const simulateComputerTurn = (validCells) =>{
   const randomCell = Math.floor(Math.random() * validCells.length);
-  
   timer(500).subscribe(()=>computerMove$.next(validCells[randomCell]));
 }
